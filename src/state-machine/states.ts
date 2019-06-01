7bc73b2d544888ea227fd12522bf32358180a4f0
albertortizn26 <albert.ortiz@n26.com>
2019-06-01 08:35:25 +0200

    Change abstract class by interface in CircuitBreakerState

--- src/state-machine/states.ts
+++ src/state-machine/states.ts
@@ -1,12 +1,11 @@
 const nowSupplier = () => new Date();
 
-abstract class CircuitBreakerState {
-    public abstract isCallPermitted(): boolean;
+interface CircuitBreakerState {
+    isCallPermitted(): boolean;
 }
 
-class ClosedCircuit extends CircuitBreakerState {
+class ClosedCircuit implements CircuitBreakerState {
     constructor(readonly failCount: number = 0) {
-        super();
     }
 
     public static start = () => new ClosedCircuit();
@@ -16,20 +15,15 @@ class ClosedCircuit extends CircuitBreakerState {
     public isCallPermitted = () => true;
 }
 
-class OpenCircuit extends CircuitBreakerState {
+class OpenCircuit implements CircuitBreakerState {
     constructor(readonly openedAt: Date) {
-        super();
     }
 
     public tryReset = () => new HalfOpenCircuit();
     public isCallPermitted = () => false;
 }
 
-class HalfOpenCircuit extends CircuitBreakerState {
-    constructor() {
-        super();
-    }
-
+class HalfOpenCircuit implements CircuitBreakerState {
     public trip = (now = nowSupplier()) => new OpenCircuit(now);
     public reset = () => ClosedCircuit.start();
     public isCallPermitted = () => true;
